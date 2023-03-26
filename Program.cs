a90977e511b804f0532a6de050ea5a71d91b35e6
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-03-26 12:04:52 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// 
+
+String Palavra1;
+String Palavra2;
+String Palavra3;
+
+Console.WriteLine("----Alinmhando a Direita----");
+Console.WriteLine();
+
+Console.Write("Digite a Primeira palavra..:");
+Palavra1 = Console.ReadLine()!.Trim();
+
+Console.Write("Digite a segunda palavra..:");
+Palavra2 = Console.ReadLine()!.Trim();
+
+Console.Write("Digite a terceira palavra..:");
+Palavra3 = Console.ReadLine()!.Trim();
+
+Console.WriteLine();
+Console.WriteLine($"{Palavra1.PadLeft(20, ' ')}");
+Console.WriteLine($"{Palavra2.PadLeft(20, ' ')}");
+Console.WriteLine($"{Palavra3.PadLeft(20, ' ')}");
+
+
